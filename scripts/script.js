ca88f86eb2cc5de04a9c9c2891eb4f709c89dcc2
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-06-14 15:39:23 -0400

    added menu bar

--- scripts/script.js
+++ scripts/script.js
@@ -5,3 +5,12 @@ $(document).ready(function(){
         $("#panel").slideToggle("slow");
     });
 });
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
