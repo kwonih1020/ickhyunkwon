bc6e23475487f1d0ccf8bde925594f7c93ce54b8
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-06-19 10:37:38 -0400

    edited font, resume, and project pages

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,38 @@ $(document).ready(function(){
     $("#flip").click(function(){
         $("#panel").slideToggle("slow");
     });
+    $("#project_pic1").on({
+        mouseenter: function() {
+            $("#project_pic1").attr("src", "./images/gif/simon_game.gif"); //on hover replace pic.png to pic.gif(animation
+          },
+          mouseleave: function() {
+            $("#project_pic1").attr("src", "./images/screenshots/simon_game.png"); //change pic.gif to pic.png
+          }
+        });
+    $("#project_pic2").on({
+        mouseenter: function() {
+            $("#project_pic2").attr("src", "./images/gif/travel_dairy.gif"); //on hover replace pic.png to pic.gif(animation
+          },
+          mouseleave: function() {
+            $("#project_pic2").attr("src", "./images/screenshots/travel_dairy.png"); //change pic.gif to pic.png
+          }
+        });
+    $("#project_pic3").on({
+        mouseenter: function() {
+            $("#project_pic3").attr("src", "./images/gif/weatro.gif"); //on hover replace pic.png to pic.gif(animation
+          },
+          mouseleave: function() {
+            $("#project_pic3").attr("src", "./images/screenshots/weatro.png"); //change pic.gif to pic.png
+          }
+        });
+    $("#project_pic4").on({
+        mouseenter: function() {
+            $("#project_pic4").attr("src", "./images/gif/tick_tac_toe.gif"); //on hover replace pic.png to pic.gif(animation
+          },
+          mouseleave: function() {
+            $("#project_pic4").attr("src", "./images/screenshots/tick_tac_toe.png"); //change pic.gif to pic.png
+          }
+        });
 });
 
 function openNav() {
